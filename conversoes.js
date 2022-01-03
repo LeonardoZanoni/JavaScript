63e4ad1c5d93f0234861e877f7fea00895d0f302
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-03 10:13:29 -0300

    Update

--- conversoes.js
+++ conversoes.js
@@ -12,3 +12,13 @@ console.log(numero + numeroString)
 // conversão explícita
 
 console.warn(new Error('oi'))
+
+
+
+
+
+// conversão de string para numero utilizando o operador soma
+
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
