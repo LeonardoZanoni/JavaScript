f0829e8a839e1923aba989efa37168e0ef769b88
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-03 17:50:15 -0300

    Update funcoes

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+//let x = "";
+//console.log(x)
+//x = "oi";
+
+//  DECLARAÇÃO DE FUNCÇÃO
+
+// 1) declara a função
+                     //string
+function imprimeTexto(texto){
+    console.log(texto)    
+}   
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+// três formas de escrever funções
+
+function soma(){
+    return  2 + 2;
+}
+console.log(soma())
