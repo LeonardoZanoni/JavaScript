9faf93f7f56b116d5230bd35817daeb139d248fe
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-02 23:33:35 -0300

    Novos exercicios

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const N1 = 5;
+const N2 =10; 
+let cadastroAtivado = true;  
+
+const leo = 25;
+const leo2 = "25";
+
+console.log(leo == leo2);
+
+console.log(N1 === N2)
---
+++ type-string.js
@@ -0,0 +1,27 @@
+const texto1 = "Olá, MUNDO!!";
+const texto2 = 'Olá, MUNDO!!';
+const StringdeNumeros = "548625";
+const citacao = "Meu nome é ";
+const meuNOME = "Leonardo";
+
+console.log(citacao + meuNOME)
+
+
+//Concatenação (+)
+
+//Template string OU template literal
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
