1074a9dd7a7bec501d51ce750e84af94aba37350
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-03 14:31:18 -0300

    Exercicios

---
+++ ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 20;
+
+//if (idadeCliente >= idadeMinima) {
+//    console.log("CERVEJA");
+
+//} else {
+//    console.log("SUCO");
+//}
+
+
+              //CONDIÇÃO                  //TRUE     //FALSE
+console.log(idadeCliente >= idadeMinima ? "CERVEJA" : "SUCO")
