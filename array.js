3f37d6b966e4e1c53a2e4146c664facbc0752b1a
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-04 13:39:27 -0300

    Array e calcular media

---
+++ array.js
@@ -0,0 +1,30 @@
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+
+// utilizando array
+
+
+  // indice  =   0   1   2
+const numeros = [50, 43, 12];
+
+
+
+// Array de numeros e strings
+
+          // indice  =   0   1   2
+const arrayDeNumeros = [50, 43, 12];
+const arrayDeStrings = ["banana", "alura", "Juliana"];
+
+
+// índice     0   1   2   3
+const nums = [50, 43, 12, 98];
+
+
+// índice     0   1   2   3
+const nums = [50, 43, 12, 98];
+console.log(nums.length) //4
+
+//length conta a quantidade de elementos inseridos no array
+
+
