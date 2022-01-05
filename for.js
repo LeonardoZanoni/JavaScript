829545712f0c798e1f389227f14a18f625a51219
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-05 15:13:23 -0300

    Add medir-for e update for

--- for.js
+++ for.js
@@ -1,7 +1,7 @@
-const numeros =[100, 200, 300, 400, 500, 600]
+const numeros = [100, 200, 300, 400, 500, 600]
 
 for (let i = 0; i < numeros.length; i++) {
 
-       //inicio 0,         0
-    console.log(i, numeros[i])    
+    //inicio 0,         0
+    console.log(i, numeros[i])
 }
