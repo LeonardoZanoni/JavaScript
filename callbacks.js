f3c87a891dce101b55d7db000bbb545a5933bc3a
Leonardo Zanoni Garcia <leo.zanoni@hotmail.com>
2022-01-06 16:19:51 -0300

    att

--- callbacks.js
+++ callbacks.js
@@ -2,6 +2,6 @@ const nomes = ["Ana", "Ju", "Leo", "Emily"];
 
 nomes.forEach(imprimeNomes)
 
-function imprimeNomes(nomes) {
-    console.log(nomes);
+function imprimeNomes(nome) {
+    console.log(nome);
 }
